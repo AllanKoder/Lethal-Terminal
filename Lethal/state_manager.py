b5d0fdeabe6ca7e1a064f7a541bd51c3c626416c
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2025-01-06 20:38:33 -0700

    Added a adding trap state, needs more work

--- Lethal/state_manager.py
+++ Lethal/state_manager.py
@@ -15,8 +15,9 @@ import keyboard
 class StateManager:
     def __init__(self):
         self.buffer = deque([]) # will be flushed with 'enter'
+        self.traps = set() # List of traps
 
-        self.set_state_to_gameplay()
+        self.gameplay_state()
     
     def is_typed(self, array: List[str]):
         if len(self.buffer) < len(array): 
@@ -41,31 +42,59 @@ class StateManager:
             if key.name == 'enter':
                 self.buffer.clear()
 
+            # No need for more than 50 characters in reality
             if len(self.buffer) > 50:
                 self.buffer.popleft()
-            # No need for more than 50 characters in reality
 
         keyboard.on_press(add_to_buffer, suppress=suppress)
+    
+    def keyboard_setup(func):
+        def decorator(self):
+            keyboard.unhook_all()
+        
+            func(self)
+        return decorator
 
-    def set_state_to_gameplay(self):
-        keyboard.unhook_all()
+    def handle_control_c(self):
+        if self.is_typed(['ctrl', 'c']):
+            self.terminal_state()
+
+    @keyboard_setup
+    def gameplay_state(self):
         print("gaming")
 
         def handle_transitions():
             # Enter Terminal State
             if self.is_typed(['t', 'enter']):
-                self.set_state_to_terminal()
+                self.terminal_state()
 
         self.listen_to_keyboard(handle_transitions, False)
 
-    def set_state_to_terminal(self):
-        keyboard.unhook_all()
+    @keyboard_setup
+    def terminal_state(self):
 
         print("terminal")
 
         def handle_transitions():
             # Enter Gameplay State
             if self.is_typed(['tab', 'tab']):
-                self.set_state_to_gameplay()
+                self.gameplay_state()
+            if self.is_typed(['a']):
+                self.add_trap_state()
+            self.handle_control_c()
+
+        self.listen_to_keyboard(handle_transitions, True)
+
+    @keyboard_setup
+    def add_trap_state(self):
+        print("add trap state")
+
+        def handle_transitions():
+            if (len(self.buffer) >= 3 and self.buffer[-1] == 'enter'):
+                trap = self.buffer[-2] + self.buffer[-3]
+                self.traps.add(trap)
+                print(trap)
+
+            self.handle_control_c()
 
         self.listen_to_keyboard(handle_transitions, True)
