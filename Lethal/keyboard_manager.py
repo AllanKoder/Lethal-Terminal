3aa0bf94a41b51430c4386beabd29e5ec5a5c3d6
TheSaddestBread <74692833+AllanKoder@users.noreply.github.com>
2025-01-08 18:04:22 -0700

    The program can now halt as expected

--- Lethal/keyboard_manager.py
+++ Lethal/keyboard_manager.py
@@ -8,11 +8,12 @@ class KeyboardManager:
     def __init__(self):
         self.queue = deque()
         self.lock = threading.Lock()
+        self.running = True  # keep running or end?
         self.thread = threading.Thread(target=self.process_keys)
         self.thread.start()
 
     def process_keys(self):
-        while True:
+        while self.running:
             with self.lock:
                 if len(self.queue) > 0:
                     key = self.queue.popleft()
@@ -24,13 +25,19 @@ class KeyboardManager:
                 sleep(INPUT_DELAY)
                 keyboard.release(key)
             else:
-                sleep(0.01)  # Sleep briefly to stop overloading the system
+                sleep(0.01)  # Sleep briefly to avoid overloading
 
     def press_key(self, key):
         with self.lock:
             self.queue.append(key)
-    
+
+    def stop(self):
+        self.running = False 
+        # Wait for the thread to finish
+        self.thread.join()
+
     def wait(self):
+        # Wait for a keyboard event
         keyboard.wait()
 
 
--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from Lethal.terminal_state_manager import TerminalStateManager
 from Lethal.keyboard_manager import KeyboardManager
 
 if __name__ == "__main__":
-    keyboard_manager = KeyboardManager()
-    manager = TerminalStateManager(keyboard_manager)
-    keyboard_manager.wait()  # Keep the program running
+    try:
+        keyboard_manager = KeyboardManager()
+        state_manager = TerminalStateManager(keyboard_manager)
+        keyboard_manager.wait()  # Keep the program running
+    finally:
+        keyboard_manager.stop()
